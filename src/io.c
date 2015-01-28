8007d834b0dc2d35c6ab4d26aeefe716ae5c8541
Ryan Hileman <lunixbochs@gmail.com>
2015-01-27 23:38:54 -0800

    implement buffered file IO; add some FILE methods

--- src/io.c
+++ src/io.c
@@ -4,12 +4,104 @@
 #include "stdlib.h"
 #include "syscall.h"
 
-static int putc(int c) {
-    return write(1, &c, 1);
+static FILE _stderr = {.fd = 2};
+static FILE _stdin = {.fd = 0};
+static FILE _stdout = {.fd = 1};
+FILE *stderr = &_stderr;
+FILE *stdin = &_stdin;
+FILE *stdout = &_stdout;
+
+static void freset(FILE *f) {
+    f->size = 0;
+    f->pos = 0;
+    memset(f->buf, 0, BUFSIZE);
 }
 
-static int puts(const char *s) {
-    return write(1, s, strlen(s));
+static int buf_write(FILE *f, const char *s, int len) {
+    // TODO: EOF + error handling
+    if ((len + f->size) > BUFSIZE) {
+        fflush(f);
+        return write(f->fd, s, len);
+    }
+    memcpy(f->buf + f->size, s, len);
+    f->size += len;
+    if (memchr(f->buf, '\n', f->size) != 0) {
+        return fflush(f) - f->size;
+    }
+    return len;
+}
+
+static int buf_read(FILE *f, char *d, int len) {
+    // TODO: EOF + error handling
+    int ret = 0;
+    int size = f->size - f->pos;
+    char *pos = f->buf + f->pos;
+    int count;
+    // TODO: handle len > bufsize
+    if (size > 0) {
+        if (len < size) count = len;
+        else            count = size;
+        memcpy(d, pos, count);
+        d += count;
+        ret += count;
+        len -= count;
+        size -= count;
+        if (count < size) {
+            f->pos += count;
+            pos += count;
+        } else {
+            freset(f);
+            pos = f->buf;
+        }
+    }
+    if (len > BUFSIZE) {
+        // TODO: error precedence over length?
+        return ret + read(f->fd, d, len);
+    } else if (len > 0) {
+        count = read(f->fd, pos, BUFSIZE);
+        if (count < 0) {
+            return count;
+        } else if (count == 0) {
+            if (ret > 0) return ret;
+            else         return -1;
+        } else if (count > len) {
+            memcpy(d, pos, len);
+            memmove(f->buf, f->buf + len, count);
+            f->size = count - len;
+            return ret + len;
+        } else {
+            memcpy(d, f->buf, count);
+            freset(f);
+            return ret + count;
+        }
+    }
+}
+
+int fflush(FILE *f) {
+    if (f->size == 0) return 0;
+    int ret = write(f->fd, f->buf, f->size);
+    freset(f);
+    return ret;
+}
+
+int fgetc(FILE *f) {
+    unsigned char c;
+    int ret = buf_read(f, &c, 1);
+    if (ret < 1) return -1;
+    return c;
+}
+
+int fputc(int c, FILE *f) {
+    unsigned char _c = c;
+    return buf_write(f, &_c, 1);
+}
+
+int putc(int c, FILE *f) {
+    return fputc(c, f);
+}
+
+int puts(const char *s) {
+    return buf_write(stdout, s, strlen(s));
 }
 
 int printf(const char *fmt, ...) {
@@ -30,10 +122,10 @@ int printf(const char *fmt, ...) {
             }
             switch (c) {
                 case '%':
-                    putc(c);
+                    putc(c, stdout);
                     break;
                 case 'c':
-                    putc(arg(int));
+                    putc(arg(int), stdout);
                     break;
                 case 's':
                     puts(arg(char *));
@@ -64,7 +156,7 @@ int printf(const char *fmt, ...) {
         } else if (c == '%') {
             control = true;
         } else {
-            putc(c);
+            putc(c, stdout);
         }
     }
     va_end(params);
--- src/io.h
+++ src/io.h
@@ -1,1 +1,17 @@
+#define BUFSIZE 4096
+
+typedef struct {
+    int fd, pos, size;
+    char buf[BUFSIZE];
+} FILE;
+
+extern FILE *stderr;
+extern FILE *stdin;
+extern FILE *stdout;
+
+int fflush(FILE *f);
+int fgetc(FILE *f);
+int fputc(int c, FILE *f);
 int printf(const char *fmt, ...);
+int putc(int c, FILE *f);
+int puts(const char *s);
