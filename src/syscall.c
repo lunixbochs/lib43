037c47278e937d39213d8117139624d36cd171c3
Travis McArthur <mcartt@amazon.com>
2015-01-28 18:15:11 -0800

    Add support for OSX x86_64 syscalls

--- src/syscall.c
+++ src/syscall.c
@@ -1,7 +1,16 @@
 #include <sys/syscall.h>
 
+/* Apple support is x86_64 only */
 #if defined(__APPLE__)
-#error "osx"
+#define sys(name) \
+    int name() {  \
+        __asm__ __volatile__(       \
+            "movq %0, %%rax;"       \
+            "syscall;"              \
+            ::"i"(SYS_##name + 0x2) \
+            :"rax"                  \
+       );                           \
+    }
 #elif defined(__linux__)
 #define sys(name) \
     int name() {  \
