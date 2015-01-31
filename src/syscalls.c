a4ddf8a41c57fb1299eae9dc4bac528298c1c86a
Ryan Hileman <lunixbochs@gmail.com>
2015-01-31 15:26:34 -0800

    fix mmap on 32-bit linux

--- src/syscalls.c
+++ src/syscalls.c
@@ -7,7 +7,11 @@ abi_long _read(int fd, const void *buf, abi_long len) {
     return syscall3(SYS_read, fd, buf, len);
 }
 void *_mmap(void *addr, abi_long len, int prot, int flags, int fd, abi_long off) {
-    return (void *)syscall6(SYS_mmap, addr, len, prot, flags, fd, off);
+    int num = SYS_mmap;
+#ifdef SYS_mmap2
+    num = SYS_mmap2;
+#endif
+    return (void *)syscall6(num, addr, len, prot, flags, fd, off);
 }
 int _munmap(void *addr, abi_long len) {
     return syscall2(SYS_munmap, addr, len);
