eb463e2288a601cb9cb35f81fc30c6394ab10328
Ryan Hileman <lunixbochs@gmail.com>
2015-01-30 22:13:53 -0800

    fix i386 _start argv

--- arch/i386/_start.s
+++ arch/i386/_start.s
@@ -1,15 +1,19 @@
 .text
 .global _start
 _start:
+    xor %ebp, %ebp
     pop %ecx
+    mov %esp, %eax
     and $-16, %esp
-    push %esp
+    push %eax
     push %ecx
     call main
 
     push %eax
+    call exit
 
     push stdout
     call fflush
+    pop %ecx
 
     call exit
