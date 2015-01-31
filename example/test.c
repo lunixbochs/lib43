c0c3b8aa6d3888514d013651fa2031b6c4d1da02
Ryan Hileman <lunixbochs@gmail.com>
2015-01-30 18:08:58 -0800

    improve _start handling (adds argv support)

--- example/test.c
+++ example/test.c
@@ -1,6 +1,10 @@
 #include "io.h"
 
-int main() {
+int main(int argc, char **argv) {
     printf("hello with printf: %s\n", "world");
+    printf("argv:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
     return 0;
 }
--- src/_start.c
+++ src/_start.c
@@ -1,7 +1,18 @@
 #include "syscall.h"
+#include "io.h"
 
 extern int main();
 
 void _start() {
-    exit(main());
+    int ret;
+    __asm__ __volatile__ (
+         "pop %%rbp;" // C compiler will push rbp
+         "pop %%rdi;"        // argc
+         "mov %%rsp, %%rsi;" // argv
+         "andq $-16, %%rsp;"
+         "call main;"
+         :"=r"(ret)
+    );
+    fflush(stdout);
+    exit(ret);
 }
