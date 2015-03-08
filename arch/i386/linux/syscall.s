9e8a650df470f0197526df2833fd42107dbe4b6e
Ryan Hileman <lunixbochs@gmail.com>
2015-03-08 14:24:55 -0700

    fix x86 linux (fixes #15)

--- arch/i386/linux/syscall.s
+++ arch/i386/linux/syscall.s
@@ -5,17 +5,20 @@ syscall:
     push %edx
     push %esi
     push %edi
+    push %ebp
 
-    add $24, %esp
-    pop %eax
+    add $28, %esp
     pop %ebx
     pop %ecx
     pop %edx
     pop %esi
     pop %edi
+    pop %ebp
+    pop %eax
     int $0x80
 
-    sub $48, %esp
+    sub $56, %esp
+    pop %ebp
     pop %edi
     pop %esi
     pop %edx
